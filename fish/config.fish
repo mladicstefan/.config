71dcdbfeadc08e73ffce9602fada7603cdb6e679
mladicstefan <mladicstefan6@gmail.com>
2025-07-22 01:48:09 +0200

    wowowo

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     set -U fish_greeting (echo -e "\033[1;32mWelcome back, \033[1;34msir\033[0m! ")
 end
 
+alias n='nvim'
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/djamla/anaconda3/bin/conda
